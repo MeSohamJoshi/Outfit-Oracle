8a2bc9e0e0ac05fd5b9dd4cd908aec7cf5bc9b28
carsonblee <carson.b.lee@icloud.com>
2025-02-26 19:22:09 -0500

    Added navbar

---
+++ Frontend/my-react-app/app/navbar.tsx
@@ -0,0 +1,29 @@
+import './navbar.css';
+
+const Navbar = () => {
+    return (
+  
+  <nav className="navbar">
+    <div className="navbar-left">
+      <a href="/" className="logo">
+        OutfitOracle
+      </a>
+    </div>
+    <div className="navbar-center">
+      <ul className="nav-links">
+        <li>
+          <a href="/products">Products</a>
+        </li>
+        <li>
+          <a href="/about">About Us</a>
+        </li>
+        <li>
+          <a href="/contact">Contact</a>
+        </li>
+      </ul>
+    </div>
+  </nav>
+  );
+  };
+  
+  export default Navbar;
