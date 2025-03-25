d93ecdd41d33410489aeb4c8c59911bc67872b79
Ajju <abhamre50@gmail.com>
2025-03-25 13:52:34 -0400

    tested-login-module

--- Backend/data/users.js
+++ Backend/data/users.js
@@ -1,32 +1,52 @@
 import { users } from "../config/mongoCollections.js";
 import admin from "firebase-admin";
 import { ObjectId } from "mongodb";
+import bcrypt from "bcrypt";
+import {
+  checkIsProperFirstOrLastName,
+  validateEmail,
+  checkIsProperPassword,
+} from "../helpers.js";
 
 const createUser = async (firstName, lastName, email, password) => {
+  firstName = checkIsProperFirstOrLastName(firstName, "First name");
+  lastName = checkIsProperFirstOrLastName(lastName, "Last name");
+  email = validateEmail(email);
+  password = checkIsProperPassword(password);
+
   const userCollection = await users();
   const existingUser = await userCollection.findOne({ email: email.trim() });
 
   if (existingUser) throw new Error("User already exists");
-  const userRecord = await admin.auth().createUser({
-    email: email,
-    password: password,
-    displayName: `${firstName} ${lastName}`,
-  });
+  try {
+    const userRecord = await admin.auth().createUser({
+      email: email,
+      password: password,
+      displayName: `${firstName} ${lastName}`,
+    });
 
-  const toAddUser = {
-    _id: new ObjectId(),
-    firstName,
-    lastName,
-    email,
-  };
+    let hashedPassword = await bcrypt.hash(password, 10);
 
-  let addedUser = await userCollection.insertOne(toAddUser);
+    const toAddUser = {
+      _id: new ObjectId(),
+      name: `${firstName} ${lastName}`,
+      email,
+      password: hashedPassword,
+      createdAt: new Date(),
+      firebaseUid: userRecord.uid,
+    };
 
-  if (!addedUser.acknowledged || !addedUser.insertedId) {
-    throw new Error(`The user could not be added to the collection.`);
+    let addedUser = await userCollection.insertOne(toAddUser);
+
+    if (!addedUser.acknowledged || !addedUser.insertedId) {
+      throw new Error(`The user could not be added to the collection.`);
+    }
+
+    return { ...toAddUser, _id: addedUser.insertedId };
+  } catch (error) {
+    console.error(`Error creating user: ${error.message}`);
+    throw error;
   }
-
-  return { ...toAddUser, _id: addedUser.insertedId };
 };
 
 const verifyLogin = async (token) => {
@@ -37,9 +57,9 @@ const verifyLogin = async (token) => {
   return {
     message: "Token verified successfully",
     _id: user._id,
-    name: `${user.firstName} ${user.lastName}`,
+    name: user.name,
     email,
   };
 };
 
-export default { createUser };
+export default { createUser, verifyLogin };
