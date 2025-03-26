29af01fe08bec403d45cccc77f2aaa8dda2b1805
carsonblee <carson.b.lee@icloud.com>
2025-03-26 18:57:52 -0400

    Updated basic login page

--- Frontend/app/routes/login.tsx
+++ Frontend/app/routes/login.tsx
@@ -26,7 +26,7 @@ const LoginPage: React.FC = () => {
         <section className="bg-gray-50 dark:bg-gray-900">
             <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0"
             style={{ maxWidth: '400px', margin: '0 auto', padding: '20px' }}>
-            <a href="#" className="flex items-center mb-6 text-2xl font-semibold text-gray-900 dark:text-white">
+            <a href="/dashboard" className="flex items-center mb-6 text-2xl font-semibold text-gray-900 dark:text-white">
                 Outfit Oracle    
                 </a>
         <div className="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
